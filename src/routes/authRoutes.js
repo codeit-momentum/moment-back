f01f6f333ea61b36abd6e26f357ca8edb605e517
kangyeon9525 <kangyeon9525@gmail.com>
2025-01-11 22:51:13 +0900

    feat - 카카오 로그인 구현

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { redirectToKakaoLogin, handleKakaoCallback, handleKakaoUser, kakaoLogin } from '../controllers/authControllers.js';
+const router = express.Router();
+
+
+router.get('/kakao', redirectToKakaoLogin); // 카카오 로그인 페이지로 리디렉션 (프론트에서 하는 작업) (테스트용)
+router.get('/kakao/callback', handleKakaoCallback); // 카카오에서 인가 코드를 수신하는 콜백 URL (프론트에서 하는 작업) (테스트용)
+
+router.post('/kakao-login', kakaoLogin); // 카카오 액세스 토큰 요청
+router.post('/kakao-login/user', handleKakaoUser); // 카카오 사용자 정보 처리
+
+export default router;
