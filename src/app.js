cda0100e8259b68c111b7d370aac6bdcab5bde58
ydjwls <xqhfkfpalx@naver.com>
2025-01-22 01:29:33 +0900

    feat: 홈 기능 구현 완료 (목록 조회, 알림 조회, 읽음 처리) & 모멘트 버킷리스트 코드 작성

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import friendRoutes from './routes/friendRoutes.js';
 import homeRoutes from "./routes/homeRoutes.js";
+import momentRoutes from "./routes/momentRoutes.js";
 
 
 dotenv.config({override: true});
@@ -21,7 +22,8 @@ app.use(cookieParser());
 app.use('/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/friends', friendRoutes);
-app.use('/api/home', homeRoutes)
+app.use('/api/home', homeRoutes);
+app.use('/api/moment', momentRoutes);
 
 console.log('Current Environment:', process.env.NODE_ENV);
 console.log('Redirect URI:', process.env.REDIRECT_URI);
