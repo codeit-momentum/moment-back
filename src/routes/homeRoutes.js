fc07507e24d1e7aac53fc124912ac9eaea59f87d
sanghyeok8473 <billy10953@naver.com>
2025-01-22 14:18:43 +0900

    home 라우트 경로 수정

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -10,11 +10,11 @@ router.use(jwtMiddleware);
 // prisma 미들웨어 적용 
 router.use(prismaMiddleware);
 
-router.get('/home', getHome); // 홈 목록 조회
-router.get('/home/notifications', getNotifications); // 알림 목록 조회 및 새 알림 표시
-router.patch('/home/notifications/{notificationsID}', markNotificationAsRead); // 알림 읽음 수정 
-router.get('/home/momentsComplete/week', getCompletedMomentsByWeek); // 요일별 인증 확인 
-router.get('/home/momentsComplete/day', getCompletedMomentsByDay); // 당일 인증 확인 
-router.get('/home/bucket', getBucketListStatus); // 버킷리스트 달성 현황
+router.get('/', getHome); // 홈 목록 조회
+router.get('/notifications', getNotifications); // 알림 목록 조회 및 새 알림 표시
+router.patch('/notifications/{notificationsID}', markNotificationAsRead); // 알림 읽음 수정 
+router.get('/momentsComplete/week', getCompletedMomentsByWeek); // 요일별 인증 확인 
+router.get('/momentsComplete/day', getCompletedMomentsByDay); // 당일 인증 확인 
+router.get('/bucket', getBucketListStatus); // 버킷리스트 달성 현황
 
 export default router;
