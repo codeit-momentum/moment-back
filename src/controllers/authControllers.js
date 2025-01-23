dd8302724bcaf9d0b6ecbe4328f3860eb32940eb
kangyeon9525 <kangyeon9525@gmail.com>
2025-01-23 14:41:51 +0900

    fix - cookie 설정 수정 samestie = none

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -150,7 +150,7 @@ export const handleKakaoUser = async (req, res) => {
         const refreshToken = generateRefreshToken(user);
 
         // Refresh Token을 HttpOnly 쿠키에 저장
-        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: false, sameSite: 'strict' });
+        res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: false, sameSite: 'none' });
 
         // 사용자 정보 + JWT 토큰 반환
         res.json({ 
